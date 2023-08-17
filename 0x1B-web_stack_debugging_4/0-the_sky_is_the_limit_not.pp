24a7f973655a990b6a9a29d050f5aa15477e1306
Douglas Nyakundi <douglasmigiro@gmail.com>
2023-08-16 22:03:51 -0700

    debugging

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,15 @@
-# Fixing the number of failed requests to get to 0
-exec { 'fix--for-nginx':
-  command => "sed -i 's/worker_processes 4;/worker_processes 7;/g' /etc/nginx/nginx.conf; sudo service nginx restart",
-  path    => ['/bin', '/usr/bin', '/usr/sbin']
+# Adjusts the worker_processes in nginx.conf to improve request handling.
+
+exec { 'update-nginx-worker-processes':
+  command => "sed -i 's/worker_processes 4;/worker_processes 7;/g' /etc/nginx/nginx.conf",
+  onlyif  => "grep -q 'worker_processes 4;' /etc/nginx/nginx.conf",
+  path    => ['/bin', '/usr/bin'],
+} ->
+
+exec { 'restart-nginx':
+  command => 'service nginx restart',
+  path    => ['/sbin', '/usr/sbin', '/usr/bin'],
 }
+
 
 
